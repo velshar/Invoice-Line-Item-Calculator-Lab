f8e1264f34d51bd7ee5fdcb648a39a44a8ef86c7
strayer_practice@outlook.com <strayer_practice@outlook.com>
2023-02-07 17:28:48 -0800

    Invoice Line Item Calculator Lab

--- Invoice_Line_Item_Calculator_Lab.py
+++ Invoice_Line_Item_Calculator_Lab.py
@@ -1,1 +1,43 @@
 
+
+def get_price():
+    while True:
+        try:
+            price = float(input("Enter price: "))
+            return price
+        except ValueError:
+            print("Invalid decimal number. Please try again.")
+
+def get_quantity():
+    while True:
+        try:
+            quantity = int(input("Enter quantity: "))
+            return quantity
+        except ValueError:
+            print("Invalid integer. Please try again.")
+
+def main():
+    print("The Invoice Line Item Calculator\n")
+
+    answer = "y"
+    while answer.lower() == "y":
+
+        price = get_price()
+        quantity = get_quantity()
+
+
+        total = price * quantity
+
+
+        print()
+        print("PRICE:     ", f"{price: .2f}")
+        print("QUANTITY:  ", quantity)
+        print("TOTAL:     ", f"{total: .2f}")
+        answer = input("Enter another line item? (y/n): ")
+        print()
+
+    print("Bye!")
+
+
+if __name__ == "__main__":
+    main()
